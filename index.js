d91ab7a13b80dddefaf00ab13a7112ea22461f34
Sara Hopper <sara.hopper@gmail.com>
2017-10-02 16:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = '760869047ff1c29982d033c65ce549b346dfeebc'
+fetch('https://api.github.com/user/repos')
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
